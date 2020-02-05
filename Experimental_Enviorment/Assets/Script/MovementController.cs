fe2472884c59dca9eeaae5e133ebc73729d45d52
Quintin Yu <Quintinyu02072@gmail.com>
2020-02-05 23:06:56 +0100

    Initial Unity Commit

---
+++ Experimental_Enviorment/Assets/Script/MovementController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float speed;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity = Vector3.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            velocity = Vector3.back * speed;
+        }
+        else
+        {
+            velocity = Vector3.zero;
+        }
+    }
+}
